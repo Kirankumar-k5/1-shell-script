df1aeb07b8c00ef81092990ff8e6780bf8d5f9b4
Kirankumar-k5 <kirankalepu1998@gmail.com>
2026-03-06 13:51:51 +0530

    complete

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Please enter your username ::"
-read USER_NAME
+read USER_NAME # USER_NAME is assign the Please enter your username ::
 
 echo "username is $USER_NAME"
 
 echo "enter your password"
-read -s PASSWORD
+read -s PASSWORD # -s for hide the password
+
+# dynamically passing the args and values
---
+++ 06-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## special variables ##
+
+echo "All args passed to script: $@"
+echo "Num of vars passed to script: $#"
+echo "Script name: $0"
+echo "Present directory: $PWD"
+echo "Who is running: $USER"
+echo "Home directory of current user: $HOME"
+echo "PID of the script: $$"
+slepp 100 &
+echo "PID of the recently executed background process: $!"
