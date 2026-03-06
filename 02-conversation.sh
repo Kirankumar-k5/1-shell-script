f958885156e9600fe8a5f44eea0df20f0b448c8f
Kirankumar-k5 <kirankalepu1998@gmail.com>
2026-03-06 10:23:52 +0530

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh:: Hello Ramesh, How r u?"
+echo "Ramesh:: Hi Suresh, I am fine thanks for asking. How are you doing?"
+echo "Suresh:: doing fine. What are you learning Ramesh?"
+echo "Ramesh:: I am leaning Shell. what about you?"
